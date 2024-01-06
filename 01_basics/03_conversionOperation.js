9048bf0c2103be6009cc9be5484eadee59f67d6f
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-01-06 15:48:03 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -41,4 +41,48 @@ Note:
 // console.log(typeof stringSomeNumber);
 
 
+// ****************** OPERATIONS ***********
 
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+// let str1 = "rahul"
+// let str2 = " ambiger"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// Note: which type of data comes first conversion and operation will be performed based on that
+// console.log("2" + 2); //22
+// console.log(2 + "2"); //22
+// console.log("2" + 2 + 2); // 222
+// console.log(2 + 2 + "2"); // 42
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+// let val1 = val2 = val3 = 2+2
+// console.log(val1);
+// console.log(val2);
+// console.log(val3);
+
+
+//Pre-increment and post-increament operators
+
+//let gameCounter = 100
+// gameCounter++;
+// ++gameCounter
+// console.log(gameCounter);
+
+/*
+Refer: for prefix and postfix operation
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+Type Conversion:
+https://tc39.es/ecma262/#sec-type-conversion
+
+
+*/
