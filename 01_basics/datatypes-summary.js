93a121a5765159fcc7a1fa1c8d07d6f5b506c690
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-01-08 11:14:03 +0000

    Datatpes Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,73 @@
+/* Primitive D.T  (Call by value) 
+
+7 Types
+1. String
+2. Number
+3. null
+4. undefined
+5. Symbol
+6. BigInt
+7. Boolean
+
+
+
+→ Non Primitive D.T  (Call by reference)
+1. Arrays
+2. Objects
+3. Functions
+
+*/
+
+// Thereby, JS → Dynamically Typed Programming Language (ie, datatype of variable is decided at runtime)
+
+//Examples
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const isLoggedOut = true
+ const TempOutside = null
+let userEmail
+
+console.log(isLoggedIn)
+console.log(isLoggedOut)
+console.log(score)
+console.log(scoreValue)
+console.log(TempOutside)
+console.log(userEmail)
+
+
+//Symbol D.T → makes any value unique (even if we have provided same type and same value)
+let id = Symbol("123")
+let anotherId = Symbol("123")
+
+console.log(id === anotherId)
+
+//bigInt → when numbers are too large
+const bigNumber = 1234448374387387874n
+console.log(bigNumber)
+console.log(typeof bigNumber)
+
+//-----------------------------------------------------------------------
+
+//Arrays
+const superheros = ["ironman", "hanuman", "superman"]
+
+// Objects
+let myObj = {
+    name: "rahul",
+    age: "101"
+}
+
+//Functions
+const MyFunc = function() {
+    console.log("Hello Buddy!!")
+}
+
+console.log(typeof myObj) //object
+console.log(typeof superheros) //object
+console.log(typeof TempOutside)
+console.log(typeof scoreValue)
+console.log(typeof MyFunc)  //funtion or object funtion
+
+//Note: typeof operator → https://tc39.es/ecma262/#sec-typeof-operator
+//return type of all these Non Primitive D.T → Object
