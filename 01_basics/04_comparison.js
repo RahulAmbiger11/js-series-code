982beb3d9adb37c44fbeec69858484e61b6ad195
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-01-08 10:01:00 +0000

    Comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 >= 1)
+console.log(2 <= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+//Internally String D.T. → Number D.T (Still not preferrable)
+console.log("2" > 1)
+console.log("2" >= 1)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+
+// "===" → Strict check ie, (value + datatype) check
+console.log("2" === 2) //false
+
+
+/*
+Note: when we compare two values, the values should be of same datatype
+
+Also refer:
+https://youtu.be/sscX432bMZo?t=5644
+
+*/
