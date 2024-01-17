2395381412be8d10f5980d39a996bfd8cd1fb2f2
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-01-17 09:31:59 +0000

    Arrow functions in JS

--- 03_basics/03_arrowFunc.js
+++ 03_basics/03_arrowFunc.js
@@ -10,38 +10,53 @@ const user = {
         console.log(`${this.username} , welcome to website`)
         console.log(this)
     }
-}
+} 
 
-// user.welcomeMessage()
-// user.username = "MSD"
-// user.welcomeMessage()
+user.welcomeMessage()
+user.username = "MSD"
+user.welcomeMessage()
 
-// console.log(this)
+console.log(this)
 
 
 // In Browser JS Engine, the Global object → "Window" (** IMP IQ **)
 
 
-/* function kbc() {
+function kbc() {
     let username = "Rahul"
     console.log(this.username) //undefined  
 }
 
-kbc() */
+kbc()
 
-/* const chaiPeCharcha = function() {
+const chaiPeCharcha = function() {
     let username = "Rahul"
     console.log(this.username) //indefined
 }
 
-chaiPeCharcha() */
+chaiPeCharcha()
 
 // Arrow function
 
-let chaiPeCharcha = () => {
+l/* et chaiPeCharcha = () => {
     let username = "Rahul"
     // console.log(this.username)
     console.log(this)  // {}
 }
 
-chaiPeCharcha()
+chaiPeCharcha() */
+
+//Explicit return 
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addTwo(3,4))
+
+
+//Implicit return
+const addThree = (n1, n2, n3) =>  (n1 + n2 + n3)
+console.log(addThree(11,43,56))
+
+const returnObj = () => ({id: 1, username: "Rahul"})
+console.log(returnObj())
