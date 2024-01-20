32cb455fb2854e790dc4e29fff266cc11ed96738
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-01-20 09:29:11 +0000

    while and do-while loops in JS

---
+++ 05_iterations/loops_2.js
@@ -0,0 +1,33 @@
+// loops in JS
+
+// while loop
+
+let index = 0
+
+while (index <= 10) {
+    console.log(`Value of index is: ${index}`)
+    index = index + 2
+}
+
+
+let myArr = ["splash", "Batman", "Superman"]
+
+let arr = 0
+
+while (arr < myArr.length) {
+    console.log(`Value is: ${myArr[arr]}`)
+    arr = arr + 1
+    
+}
+
+// do-while loop
+
+let score = 11
+
+do {
+    console.log(`Score is: ${score}`)
+    score++
+
+} while (score <= 10);
+
+
