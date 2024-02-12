3e0a1b56e0c3525fcfc9e2463ad8b355209b740c
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-02-12 14:47:37 +0000

    project-04  GuessTheNumber

---
+++ 07_projects/04-GuessTheNumber/guess_the_number.js
@@ -0,0 +1,29 @@
+const randomNumber = parseInt(Math.random() * 100 + 1)
+
+const guessField = document.querySelector('#guessField')
+const submitGuess = document.querySelector('#subt')
+const guesses = document.querySelector('.guesses')
+const lastGuess = document.querySelector('.lastResult')
+const guessLowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+
+document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+function validateGuess(guess) {
+//
+
+}
+
+function checkGuess(guess) {
+//
+}
+
+function displayMessage(message) {
+//
+}
