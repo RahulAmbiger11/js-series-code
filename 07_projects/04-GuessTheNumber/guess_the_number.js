ed28953e1d0338f9f3f01a27435b695d2713d671
Rahul Ambiger <139382710+RahulAmbiger11@users.noreply.github.com>
2024-02-13 11:06:23 +0000

    project-04 GuessTheNumber

--- 07_projects/04-GuessTheNumber/guess_the_number.js
+++ 07_projects/04-GuessTheNumber/guess_the_number.js
@@ -1,29 +1,69 @@
 const randomNumber = parseInt(Math.random() * 100 + 1)
 
-const guessField = document.querySelector('#guessField')
-const submitGuess = document.querySelector('#subt')
-const guesses = document.querySelector('.guesses')
-const lastGuess = document.querySelector('.lastResult')
-const guessLowOrHi = document.querySelector('.lowOrHi')
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
 const startOver = document.querySelector('.resultParas')
 
 
-document.createElement('p')
+const p = document.createElement('p')
 
 let prevGuess = []
 let numGuess = 1
 
 let playGame = true
 
-function validateGuess(guess) {
-//
+if(playGame) {
+    submit.addEventListener('click', (e) => {
+        e.preventDefault()
+        const guess = parseInt(userInput.value)
+        // console.log(guess)
+        validateGuess(guess)
+    })
+}
 
+function validateGuess(guess) {
+    //
+
+    if(isNaN(guess)) {
+        alert('Please enter a valid number')
+    } else if(guess < 1) {
+        alert('Please enter a number greater than 1')
+    } else if(guess > 100){
+        alert('Please enter a number less than 100')
+    } else {
+        prevGuess.push(guess)
+        if(numGuess === 11) {
+            displayGuess(guess)
+            displayMessage(`GameOver, Random number was ${randomNumber}`)   
+            endGame()
+        }
+    }
 }
 
 function checkGuess(guess) {
-//
+    //
 }
 
-function displayMessage(message) {
-//
+function displayGuess(guess) {
+    //
 }
+
+
+function displayMessage(message) {
+    //
+}
+
+function endGame() {
+    //
+}
+
+function startGame() {
+    //
+}
+
+
+
+
